41cfa2f80015c17fc3b9aeeb2a284ab3d4f90cc4
Juan Manuel Rey Acosta <juanmanuelreyacosta@Juans-MBP.local>
2017-02-26 23:08:21 -0500

    commit 2

---
+++ angular_json_table/angular.js
@@ -0,0 +1,16 @@
+
+var app = angular.module('angularTable', []);
+app.controller('listdata',function($scope, $http){
+
+    $scope.users = []; //declare an empty array
+
+    $http.get("angular.json").success(function(response){ 
+        $scope.users = response;  //ajax request to fetch data into $scope.data
+    });
+
+    $scope.sort = function(keyname){
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    }
+
+});
