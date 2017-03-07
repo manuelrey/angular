30ec219fdc51e5e05efd2bd58982b14318b6e175
Juan Manuel Rey Acosta <juanmanuelreyacosta@Juans-MBP.local>
2017-03-06 22:54:08 -0500

    commit 5

---
+++ angular_pagination/script.js
@@ -0,0 +1,35 @@
+window.App = angular.module('App', ['ui.bootstrap', 'ngResource']);
+
+App.controller('test', ['$scope', '$resource', '$http', function($scope, $resource, $http){
+
+	console.log('test');
+	$scope.users = [];
+	$http.get("http://jsonplaceholder.typicode.com/posts/").success(function(response){
+
+		$scope.users = response;
+		console.log(response);
+
+	   $scope.totalItems = $scope.users.length;
+       $scope.predicate = 'id';  
+       $scope.reverse = true;  
+       $scope.currentPage = 1; 
+
+	
+			
+	})
+	$scope.order = function (predicate) {  
+	    $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;  
+	    $scope.predicate = predicate;  
+	  };    
+	 $scope.numPerPage = 5;  
+	 $scope.paginate = function (value) {  
+	   var begin, end, index;  
+	   begin = ($scope.currentPage - 1) * $scope.numPerPage;  
+	   end = begin + $scope.numPerPage;  
+	   index = $scope.users.indexOf(value);  
+	   return (begin <= index && index < end);  
+	 }; 
+
+
+	
+}]);
